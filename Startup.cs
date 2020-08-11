aaf6a38e4a2a152aa5fbf606834a797f4fcfebab
Shirley Atkinson <Shirley.atkinson@plymouth.ac.uk>
2020-08-11 16:40:15 +0100

    Endpoints controllers added to start up

--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,8 @@ namespace COMP2001_API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<DataAccess>();   
+            services.AddScoped<DataAccess>();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,10 +41,7 @@ namespace COMP2001_API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
